eca2695979eda2f06719c773a3412a2c74fc30ef
Podinikiya <whatsappbot299@gmail.com>
2024-06-08 23:58:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -55,8 +55,8 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "94774574323";
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
 global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,94774574323";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "94774574323";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "94743492396,94774574323";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "94743492396,94774574323";
 
 global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
 global.scan = "https://suhail-md-vtsf.onrender.com";
